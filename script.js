e2cfe8846d07c1172b9f0ce5fb66ff206bd0c480
madrodgerflynn <madrodgerflynn@gmail.com>
2022-08-06 18:59:15 -0600

    JS code done, needs ReadMe

--- script.js
+++ script.js
@@ -1,39 +1,47 @@
 // Assignment Code
+upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+lowerCase = "abcdefghijklmnopqrstuvwxyz";
+specialCharacters = "!@#$%^&*";
+numberCharacters = "0123456789";
+
+criteria = [lowerCase, upperCase, specialCharacters, numberCharacters];
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
 }
 function start() {
+  mathGenerator = "";
+  finalPassword = "";
   var userInput = prompt(
-    "Welcome to PW Generator, How many characters would you prefer out of 256?"
+    "Welcome to PW Generator, How many characters would you prefer 128?"
   );
-  //logging the users choice
-  var userChoice = userInput;
-  console.log(userChoice);
-  if ((userChoice == [8 - 128])) console.log([]);
-}
-function characters() {
-  var userInput = prompt("Now please add any special charaters");
-  var userChoice = userInput
-  console.log(userChoice);
-  if (userChoice == "!@#$%^&*()?") 
-  console.log(userChoice)
-}
-function upperLower() {
-  var userInput = confirm("Upper and lowercase?"(Boolean));
-  var userChoice = userInput
-  console.log(userChoice);
-  if (userChoice == true) 
-  console.log(userChoice)
+
+  var specChar = confirm("Would you like special characters?");
+
+  var numbChar = confirm("Would you like any numbers?");
+
+  var lowerInput = confirm("woudl you like lowercase letters?");
+  var upperInput = confirm("Would like uppercase letters?");
+
+  var valueList = [lowerInput, upperInput, specChar, numbChar];
+  for (var i = 0; i < 4; i++) {
+    if (valueList[i] == true) {
+      mathGenerator += criteria[i];
+    }
+  }
+  for (var n = 0; n < userInput; n++) {
+    finalPassword +=
+      mathGenerator[Math.floor(Math.random() * mathGenerator.length)];
+  }
+  console.log(finalPassword);
+  var passwordText = document.querySelector("#password");
+
+  passwordText.innerHTML = finalPassword;
 }
 function generatePassword() {
-  
-  
+  start();
 }
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
